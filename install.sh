4619ea757a0482c9d7fbc68bc781571eedf5e43c
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2024-11-14 00:43:03 -0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install -r requirements.txt
+git clone https://github.com/AAAAAEXQOSyIpN2JZ0ehUQ/IPTV-List-Checker.git
+cd IPTV-List-Checker
+ls -ltha
+sudo python3 -m pip install -r requirements.txt 
+sudo rm -rf wget-log*
+
+
